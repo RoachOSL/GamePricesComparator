c23bf19a59d6b50da845935b845c3dd2446d259b
RoachOSL <os1996@o2.pl>
2024-02-13 17:19:43 +0100

    Added more methods to clarity the code and overall improvements

--- src/main/java/dev/Roach/Menu.java
+++ src/main/java/dev/Roach/Menu.java
@@ -4,61 +4,32 @@ import dev.Roach.datamodel.game.Game;
 import dev.Roach.datamodel.game.GamePojo;
 import dev.Roach.fetchers.GamesFetcher;
 import dev.Roach.fetchers.StoresFetcher;
-import lombok.Setter;
 
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-@Setter
 public class Menu {
 
-    private static boolean programControlFlag = false;
+    private boolean isProgramRunning = true;
+    private final Scanner scanner = new Scanner(System.in);
+    private final StoresFetcher storesFetcher = new StoresFetcher();
+    private final GameLookup gameLookup = new GameLookup();
+    private final GamesFetcher gamesFetcher = new GamesFetcher();
+    private final AlertService alertService = new AlertService();
+
 
     public void startTheProgram() {
 
-        StoresFetcher storesFetcher = new StoresFetcher();
-        GameLookup gameLookup = new GameLookup();
-        GamesFetcher gamesFetcher = new GamesFetcher();
-        AlertService alertService = new AlertService();
-        Scanner scanner = new Scanner(System.in);
+        while (isProgramRunning) {
 
-        while (!programControlFlag) {
             storesFetcher.getAllShops();
-            System.out.println("Welcome to the Deal Finder!");
-            System.out.print("""
-                    Menu (please select an option by entering the corresponding number):
-                    1. Search for deals using an exact game title.
-                    2. Search for deals using a keyword (limit to 25 results).
-                    3. Set up a price alert for a game.
-                    4. Manage your alerts via email link.
-                    5. Exit the program.
-                    """);
+            displayMenu();
 
             try {
-
-                int choice = 0;
-
-                try {
-                    System.out.println("Please select an option:");
-                    choice = Integer.parseInt(scanner.nextLine());
-
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input. Please enter a valid number.");
-                }
-
-                switch (choice) {
-                    case 1 -> searchForDealsByTitle(scanner, gameLookup);
-                    case 2 -> searchForDealsByKeyword(scanner, gamesFetcher);
-                    case 3 -> setUpPriceAlert(scanner, alertService);
-                    case 4 -> manageAlertsByEmail(scanner, alertService);
-                    case 5 -> {
-                        programControlFlag = true;
-                        System.out.println("Exiting the program. Goodbye!");
-                    }
-                    default -> System.out.println("Invalid selection, please try again.");
-                }
+                int option = selectOption();
+                handleMenuOption(option);
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a number.");
                 scanner.nextLine();
@@ -66,9 +37,54 @@ public class Menu {
         }
     }
 
-    private void searchForDealsByTitle(Scanner scanner, GameLookup gameLookup) {
+    private String getUserInput() {
+        return scanner.nextLine().strip();
+    }
+
+    private void displayMenu() {
+        System.out.println("""
+                Welcome to the Deal Finder!
+                Menu (please select an option by entering the corresponding number):
+                1. Search for deals using an exact game title.
+                2. Search for deals using a keyword (limit to 25 results).
+                3. Set up a price alert for a game.
+                4. Manage your alerts via email link.
+                5. Exit the program.
+                """);
+    }
+
+    private void handleMenuOption(int option) {
+
+        switch (option) {
+            case 1 -> searchForDealsByTitle(gameLookup);
+            case 2 -> searchForDealsByKeyword(gamesFetcher);
+            case 3 -> setUpPriceAlert(alertService);
+            case 4 -> manageAlertsByEmail(alertService);
+            case 5 -> {
+                isProgramRunning = false;
+                System.out.println("Exiting the program. Goodbye!");
+            }
+            default -> System.out.println("Invalid selection, please try again.");
+        }
+    }
+
+    public int selectOption() {
+
+        int choice = 0;
+
+        try {
+            System.out.println("Please select an option:");
+            choice = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+        }
+
+        return choice;
+    }
+
+    private void searchForDealsByTitle(GameLookup gameLookup) {
         System.out.println("Enter the exact title of the game to find deals:");
-        String title = scanner.nextLine();
+        String title = getUserInput();
 
         try {
             System.out.println(gameLookup.giveTitleToGetListOFDealsWithStores(title));
@@ -77,12 +93,12 @@ public class Menu {
             System.out.println("Please try again with a different title.");
         }
 
-        promptToReturn(scanner);
+        promptToReturn();
     }
 
-    private void searchForDealsByKeyword(Scanner scanner, GamesFetcher gamesFetcher) {
+    private void searchForDealsByKeyword(GamesFetcher gamesFetcher) {
         System.out.println("Enter a keyword to search for game deals:");
-        String keyword = scanner.nextLine();
+        String keyword = getUserInput();
 
         List<GamePojo> gamePojos = gamesFetcher.getGameContainingKeyword(keyword);
         gamePojos.stream()
@@ -94,75 +110,82 @@ public class Menu {
             System.out.println("No game deals found for the keyword: " + keyword);
         }
 
-        promptToReturn(scanner);
+        promptToReturn();
     }
 
-    private void setUpPriceAlert(Scanner scanner, AlertService alertService) {
+    private String validateEmailInput() {
+        String email = "";
+        while (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
+            email = getUserInput();
+            if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
+                System.out.println("Invalid email format. Try again.");
+            }
+        }
+        return email;
+    }
+
+    private void setUpPriceAlert(AlertService alertService) {
         String title = "";
+
         while (title.isEmpty()) {
             System.out.println("Enter the game title for the price alert:");
-            title = scanner.nextLine().strip();
+            title = getUserInput();
             if (title.isEmpty()) {
                 System.out.println("Game title cannot be empty.");
             }
         }
 
         double price = -1;
-        while (price < 0) {
-            System.out.println("Enter the target price for the alert (alerts trigger below this price):");
-            String priceInput = scanner.nextLine().strip();
+
+        while (price < 0 || price > 1000) {
+            System.out.println("Enter the target price for the alert in dollars (alerts trigger below this price):");
+            String priceInput = getUserInput();
             try {
                 price = Double.parseDouble(priceInput);
                 if (price < 0) {
                     System.out.println("Price cannot be negative.");
+                } else if (price > 1000) {
+                    System.out.println("This price is too high for alert.");
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a valid number for the price.");
+                price = -1;
             }
         }
 
-        String email = "";
-        while (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
-            System.out.println("Enter your email to receive alert notifications:");
-            email = scanner.nextLine().strip();
-            if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
-                System.out.println("Invalid email format.");
-            }
-        }
+        System.out.println("Enter your email to receive alert notifications:");
+        String email = validateEmailInput();
 
         boolean success = alertService.createOrUpdateAlertWithGameTitle(title, email, price);
         System.out.println(success ? "Alert created successfully for " + email : "Failed to create alert for " + email);
 
-        promptToReturn(scanner);
+        promptToReturn();
     }
 
-    private void manageAlertsByEmail(Scanner scanner, AlertService alertService) {
+    private void manageAlertsByEmail(AlertService alertService) {
 
-        String email = "";
-        while (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
-            System.out.println("Enter your email to receive alert notifications:");
-            email = scanner.nextLine().strip();
-            if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}")) {
-                System.out.println("Invalid email format.");
-            }
-        }
+        System.out.println("Enter your email to receive alert notifications:");
+
+        String email = validateEmailInput();
 
         System.out.println(alertService.getAlertsForEmail(email));
 
-        promptToReturn(scanner);
+        promptToReturn();
     }
 
-    private boolean promptToReturn(Scanner scanner) {
+    private void promptToReturn() {
 
-        while (true) {
+        boolean shouldReturn = false;
+
+        while (!shouldReturn) {
             System.out.println("Press 'M/m' to return to the menu, or 'E/e' to exit.");
-            String decision = scanner.nextLine().strip().toUpperCase();
+            String decision = getUserInput().toUpperCase();
 
             if ("M".equals(decision)) {
-                return true;
+                shouldReturn = true;
             } else if ("E".equals(decision)) {
-                programControlFlag = true;
-                return false;
+                shouldReturn = true;
+                isProgramRunning = false;
             } else {
                 System.out.println("Invalid input. Please try again.");
             }
